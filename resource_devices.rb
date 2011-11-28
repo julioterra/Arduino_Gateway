cc88774c3cca74396c9e9b5eb2b17065800f1e10
Julio Terra <julioterra@gmail.com>
2011-11-28 13:18:08 -0500

    updated the databse class so that it creates datatables based on variables from each class, rather than instance methods (or even instance variables from each class).

--- resource_devices.rb
+++ resource_devices.rb
@@ -5,75 +5,65 @@ module ArduinoGateway
   module DataRecords
     
     ##########################################################################
-    # RESOURCE - abstract class for all data structure description classes
+    # AbstractRecord - abstract class for all data structure description classes
     class AbstractRecord 
       def self.data_attributes *attributes
         attributes.each do |attr|
           attr_accessor attr
-          define_method(attr.to_sym) do
-            eval "@#{attr.to_s} = 0"
-          end
+          instance_variable_set "@#{attr.to_s}", 0
         end
       end
     end
     
-    # RESOURCE DEVICE - implementation of resource device data structure description classes
+    # ResourceDevice - implementation of resource device data structure description classes
     class ResourceDevice < AbstractRecord
       data_attributes :ip, :port
     end
     
-    # RESOURCE DEVICE - implementation of resource device data structure description classes
+    # ResourceService - implementation of resource device data structure description classes
     class ResourceService < AbstractRecord
       data_attributes :id, :name
     end
 
-    # # RESOURCE DEVICE - implementation of resource device data structure description classes
-    # class ResourceDevice 
-    #   attr_accessor :ip, :port
-    # end
-    # 
-    # # RESOURCE DEVICE - implementation of resource device data structure description classes
-    # class ResourceService 
-    #   attr_accessor :id, :name
-    # end
-    # 
-    # # RESOURCE DEVICE - implementation of resource device data structure description classes
-    # class ResourceRelationships
-    #   attr_accessor :device_id, :service_id, :post_enabled, :range_max, :range_min, 
-    # end
+    # ResourceRelationship
+    class ResourceRelationship < AbstractRecord
+      data_attributes :device_id, :service_id, :post_enabled, :range_max, :range_min, 
+    end
     
   end
   
   ##########################################################################
   # DATABASE_BUILDER - wrapper class for building and managing ruport databases 
   class DatabaseBuilder
-    
+
+    @datatables = {}
+
     DataRecords.constants.each do |record_template|
       unless record_template.equal? :AbstractRecord
-      variable_name = "@#{record_template.to_s.gsub(/([a-z])([A-Z])/, "\\1_\\2").downcase}s"
+      variable_name = "#{record_template.to_s.gsub(/([a-z])([A-Z])/, "\\1_\\2").downcase}s"
       record_name = "::ArduinoGateway::DataRecords::#{record_template.to_s}"
 
-      puts "@table_columns = #{record_name}.new.public_methods(false).select! {|cur| !cur.to_s.include?('=')}"
-      puts "#{variable_name} = Ruport::Data::Table.new column_names: table_columns"
+      create_column_name = "@table_columns = #{record_name}.instance_variables.map!{|c| c.to_s.gsub!(/@/, '')}"
+      create_datatable = "@#{variable_name} = Ruport::Data::Table.new column_names: @table_columns"
+      add_table_to_array = "@datatables[:#{variable_name}] = @#{variable_name}"
 
-      eval "@table_columns = #{record_name}.new.public_methods(false).select! {|cur| !cur.to_s.include?('=')}"
-      eval "#{variable_name} = Ruport::Data::Table.new column_names: @table_columns"
-      eval "puts #{variable_name}.to_text"
+      eval create_column_name
+      eval create_datatable
+      eval add_table_to_array
 
-      else puts "skipping AbstractRecord"      
+      else puts "skipping AbstractRecord"   
       end
     end
-
-    def initialize(data_struct)
-      table_columns = data_struct.public_methods(false)
-      table = Ruport::Data::Table.new column_names: table_columns
-      puts table.to_text
-    end    
     
-    def method_missing(method_id, *args)
-      if table.respond_to?(method_id) then table.send(method_id, args) 
-      else; super; end
+    @datatables.each do |key, val|
+      puts key
+      puts val.to_text
     end
+
+    # def method_missing(method_id, *args)
+    #   if table.respond_to?(method_id) then table.send(method_id, args) 
+    #   else; super; end
+    # end
   end
 
   ##########################################################################
@@ -85,9 +75,3 @@ module ArduinoGateway
   # end
 
 end
-
-# module ArduinoGateway
-#   class ResourceDeviceRecord
-#     # data_attributes :ip, :port
-#   end
-# end
